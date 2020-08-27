b40e01abed9196e8433de58b3f6e9a3ace421390
PurimulahaSoerudake <69082673+PurimulahaSoerudake@users.noreply.github.com>
2020-08-27 11:19:22 +0900

    条件分岐・演算子
    課題提出用

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,21 @@
+//問1
+var a;
+a !== 10;
+var b;
+10 <= b && b < 20 && b % 2 ===0;
+
+//問2
+var x = 10;
+if(10 <= x && x <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+//問3
+var y = 0;
+if(y % 2 ===0){
+    console.log("偶数です");
+}else if(y % 2 !== 0){
+    console.log("奇数です");
+}
